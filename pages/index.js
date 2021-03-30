2bc0cee55594ba242ad7b2dbbaf0a8c0cf1c4add
robert <bobtomlin@gmail.com>
2021-03-30 10:27:22 -0400

    Fetch Posts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
 import Layout from '../components/Layout';
+import axios from 'axios'
 
-const Home = () => <Layout>A Gravity Well Solution</Layout>;
+const Index = ({posts}) => {
 
-export default Home;
+return(
+  <div>
+  <>
+ <Layout>Gravity Well Solutions</Layout>
+  </>
+<>
+  <ul>
+  {posts.map(post =>(
+  <li key={post.id}>
+    {post.title}
+  </li>
+))}
+  </ul>
+</>
+</div>
+
+  )
+}
+
+
+
+
+Index.getInitialProps = async ()=>{
+
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+  const {data} = res
+
+  return{posts: data} // makes it available to index (Home)
+}
+
+export default Index
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,10 @@
 import Layout from '../components/Layout';
 
-const Login = () => <Layout>login page</Layout>;
+const Login = () => {
+console.log('hello')// show up in browser console and Terminal window when refreshed
+return(
+<Layout>login page</Layout>
+)
+}
 
 export default Login;
